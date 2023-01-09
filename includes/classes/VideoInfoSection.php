65947aaa8fdc8dd09184ff2ffdaef1946c65a69b
uamafias <uainvestmentscc@gmail.com>
2023-01-09 19:40:02 +0100

    Completed till edit vid btn and subcribe btn error

--- includes/classes/VideoInfoSection.php
+++ includes/classes/VideoInfoSection.php
@@ -1,81 +1,68 @@
 <?php
 require_once("includes/classes/VideoInfoControls.php"); 
-class VideoInfoSection{
+class VideoInfoSection {
 
     private $con, $video, $userLoggedInObj;
 
-    public function __construct($con, $video, $userLoggedInObj){
-        $this->video=$video;
-        $this->con=$con;
-        $this->userLoggedInObj=$userLoggedInObj;
+    public function __construct($con, $video, $userLoggedInObj) {
+        $this->con = $con;
+        $this->video = $video;
+        $this->userLoggedInObj = $userLoggedInObj;
     }
 
-    // This create() Function is used to call all the sub-section functions that will creat the overall video information section 
-    //Containing the video title and the view count.. it simply just calls the functions that are really creating the 2 sections
-    public function create(){
-        return $this->createPrimaryInfo() . $this->createSecondaryInfo();   //This appends the 2 function calls for the different sections of the video info part
+    public function create() {
+        return $this->createPrimaryInfo() . $this->createSecondaryInfo();
     }
 
+    private function createPrimaryInfo() {
+        $title = $this->video->getTitle();
+        $views = $this->video->getViews();
 
+        $videoInfoControls = new VideoInfoControls($this->video, $this->userLoggedInObj);
+        $controls = $videoInfoControls->create();
 
-//This function is responsible for creating the first part of the video info section containing the title and video count
-//We use specialised functions from other class like the video class to collect and store data in variables than use those varribles in html elements 
-                
-private function createPrimaryInfo(){
-                        $title = $this->video->getTitle();  // Calls the getTitle function and stores the value of the title in the variable title to make use of it in the html return call
-                        $views = $this->video->getViews(); // Calls the getViews function and stores it in the views varriable to resuse it in the html tags
-                    
-                        $videoInfoControls = new VideoInfoControls($this->video, $this->userLoggedInObj);
-                        $controls = $videoInfoControls->create();
+        return "<div class='videoInfo'>
+                    <h1>$title</h1>
 
-                            return "<div class='videoInfo'>  
-                                    
-                                            <h1>$title</h1>
+                    <div class='bottomSection'>
+                        <span class='viewCount'>$views views</span>
+                        $controls
+                    </div>
+                </div>";
+    }
 
-                                            <div class='bottomSection'>
-                                            <span class='viewCount'>$views views</span>
-                                            $controls
-                                            </div>
+    private function createSecondaryInfo() {
 
-                                    </div>";  //Returns the html elements that will render the title and view count on the screen.
- }  
-                    
-    
- private function createSecondaryInfo(){
-        
         $description = $this->video->getDescription();
         $uploadDate = $this->video->getUploadDate();
         $uploadedBy = $this->video->getUploadedBy();
         $profileButton = ButtonProvider::createUserProfileButton($this->con, $uploadedBy);
+
+        if($uploadedBy == $this->userLoggedInObj->getUsername()) {
+            $actionButton = ButtonProvider::createEditVideoButton($this->video->getId());
+        }
+        else {
+            $userToObject = new User($this->con, $uploadedBy);
+            $actionButton = ButtonProvider::createSubscriberButton($this->con, $userToObject, $this->userLoggedInObj);
+        }
         
+        return "<div class='secondaryInfo'>
+                    <div class='topRow'>
+                        $profileButton
 
-        if($uploadedBy == $this->userLoggedInObj->getUsername()){
+                        <div class='uploadInfo'>
+                            <span class='owner'>
+                                <a href='profile.php?username=$uploadedBy'>
+                                    $uploadedBy
+                                </a>
+                            </span>
+                            <span class='date'>Published on $uploadDate</span>
+                        </div>
+                        $actionButton
+                    </div>
+        
+                </div>";
+    }
 
-            $actionButton = ButtonProvider:: createEditVideoButton($this->video->getId());
-        } 
-        else{
-                $qctionButton = "";
-        }
-
-    return "<div class='secondaryInfo'>
-                <div class='topRow'>
-                $profileButton
-
-                <div class = 'uploadInfo'>
-                    <span class='owner'>
-                        <a href='profile.php?username='$uploadedBy'>
-                            $uploadedBy
-                    </span> 
-                    <span class='date'>Published on $uploadDate</span>
-                </div>
-                    $actionButton
-                </div>
-             </div>";
- }
-
-
-    
-
-
-        }
+}
 ?>
